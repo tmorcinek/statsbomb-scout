2699b133cbbdcae7250139ca2fe853379a61488f
tomasz-morcinek-dtiq <TMorcinek@dtiq.com>
2026-02-13 09:25:51 +0100

    refactor: add dense layer and dropout to LSTMSequenceModel initialization

--- src/ml/models/lstm.py
+++ src/ml/models/lstm.py
@@ -4,11 +4,12 @@ from tensorflow.keras import layers
 
 class LSTMSequenceModel:
 
-    def __init__(self, input_shape: tuple, lstm_units: int = 128, dropout: float = 0.2, use_second_lstm: bool = True):
+    def __init__(self, input_shape: tuple, lstm_units: int = 128, dropout: float = 0.2, dense_units: int = 64, use_second_lstm: bool = True):
         self.input_shape = input_shape
         self.lstm_units = lstm_units
         self.dropout = dropout
         self.use_second_lstm = use_second_lstm
+        self.dense_units = dense_units
         self.model = None
 
     def build(self) -> keras.Model:
@@ -22,6 +23,9 @@ class LSTMSequenceModel:
             x = layers.Dropout(self.dropout)(x)
         else:
             x = layers.LSTM(self.lstm_units)(x)
+        if self.dense_units > 1:
+            x = layers.Dense(self.dense_units, activation='relu')(x)
+            x = layers.Dropout(self.dropout)(x)
 
         outputs = layers.Dense(1, activation='linear')(x)
         self.model = keras.Model(inputs=inputs, outputs=outputs)
